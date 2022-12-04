0f26278d7b4be37cf61b282f8235854a9326eb57
CesarJZO <cesarjzo@outlook.com>
2022-12-03 23:06:30 -0600

    Add test json files

---
+++ Assets/Scripts/JSON/PlayerInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace JSON
+{
+    [System.Serializable]
+    public class PlayerInfo
+    {
+        public string name;
+        public int lives;
+        public float health;
+
+        public static PlayerInfo CreateFromJson(string jsonString)
+        {
+            return JsonUtility.FromJson<PlayerInfo>(jsonString);
+        }
+
+        public override string ToString() => $"Name: {name}, Lives: {lives}, Health: {health}";
+    }
+}
