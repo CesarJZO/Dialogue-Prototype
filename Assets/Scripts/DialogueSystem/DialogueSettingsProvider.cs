10855120fa34f12dc252fff7e00d38e20cb5111c
César Zárate <cesarjzo@outlook.com>
2023-07-07 20:02:21 -0600

    Use UIElements

--- Assets/Scripts/DialogueSystem/DialogueSettingsProvider.cs
+++ Assets/Scripts/DialogueSystem/DialogueSettingsProvider.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
-using UnityEngine;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
 
 namespace CesarJZO.DialogueSystem
 {
@@ -15,7 +16,45 @@ namespace CesarJZO.DialogueSystem
                 {
                     SerializedObject settings = DialogueSettings.SerializedSettings;
 
+                    var title = new Label("Dialogue Settings");
+                    title.AddToClassList("title");
+                    rootElement.Add(title);
 
+                    var properties = new VisualElement
+                    {
+                        style =
+                        {
+                            flexDirection = FlexDirection.Column
+                        }
+                    };
+                    properties.AddToClassList("property-list");
+                    rootElement.Add(properties);
+
+                    var letterDelayTextField = new FloatField("Letter Delay")
+                    {
+                        value = settings.FindProperty("letterDelay").floatValue
+                    };
+                    letterDelayTextField.AddToClassList("letter-delay-field");
+                    properties.Add(letterDelayTextField);
+
+                    var dialogueDelayTextField = new FloatField("Dialogue Delay")
+                    {
+                        value = settings.FindProperty("dialogueDelay").floatValue
+                    };
+                    dialogueDelayTextField.AddToClassList("dialogue-delay-field");
+                    properties.Add(dialogueDelayTextField);
+
+                    letterDelayTextField.RegisterValueChangedCallback(evt =>
+                    {
+                        settings.FindProperty("letterDelay").floatValue = evt.newValue;
+                        settings.ApplyModifiedProperties();
+                    });
+
+                    dialogueDelayTextField.RegisterValueChangedCallback(evt =>
+                    {
+                        settings.FindProperty("dialogueDelay").floatValue = evt.newValue;
+                        settings.ApplyModifiedProperties();
+                    });
 
                     settings.ApplyModifiedProperties();
                 },
